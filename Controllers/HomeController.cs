fe9973f2a68cbbca73ef9986c2426f20a553c121
tegarsweefer <tegar.s@weefer.co.id>
2020-05-28 15:27:29 +0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkingWithString.Helpers;
+
+namespace WorkingWithString.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var randonString = Helper.RandomString(5);
+            return Ok(randonString);
+        }
+    }
+}
